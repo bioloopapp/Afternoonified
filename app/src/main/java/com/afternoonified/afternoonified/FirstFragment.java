d63d05f3ec2abdf3ac77bdd0addb8e940f9453ec
bioloopappdeveloper <bioloopapp@gmail.com>
2020-08-06 08:06:10 -0700

    Initial commit.

---
+++ app/src/main/java/com/afternoonified/afternoonified/FirstFragment.java
@@ -0,0 +1,51 @@
+package com.afternoonified.afternoonified;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        WebView myWebView = (WebView)getView().findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setSupportMultipleWindows(true);
+        //myWebView.setFocusable(true);
+        //myWebView.setFocusableInTouchMode(true);
+        myWebView.loadUrl("https://m.etsy.com/shop/shopafternoonified");
+
+        /*
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+         */
+    }
+}
---
+++ app/src/main/java/com/afternoonified/afternoonified/MainActivity.java
@@ -0,0 +1,98 @@
+package com.afternoonified.afternoonified;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (Intent.ACTION_VIEW , Uri.parse("mailto:" + "ahauntedswan@gmail.com"));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Afternoonified email");
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        WebView myWebView = (WebView)findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setSupportMultipleWindows(true);
+       // myWebView.requestFocus(View.FOCUS_DOWN);
+        //myWebView.setWebViewClient(new WebViewClient(){
+        //    @Override
+        //    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        //        return super.shouldOverrideUrlLoading(view, request);
+        //    }
+        //});
+
+        if (id == R.id.instagram_url) {
+            myWebView.loadUrl("https://www.instagram.com/shopafternoonified/");
+            return true;
+        }
+        else if (id == R.id.etsy) {
+            myWebView.loadUrl("https://m.etsy.com/shop/shopafternoonified");
+            return true;
+        }
+        else if (id == R.id.instagram_url2) {
+            myWebView.loadUrl("https://www.instagram.com/ahauntedswan");
+            return true;
+        }
+        else if (id == R.id.twitter) {
+            myWebView.loadUrl("https://mobile.twitter.com/ahauntedswan");
+            return true;
+        }
+        else if (id == R.id.kc) {
+            myWebView.loadUrl("https://www.katecatalina.com");
+            return true;
+        }
+        else if (id == R.id.exit) {
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
